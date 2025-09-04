c3376c77302acba72c8895a714884a50100f5f71
DBinK <DBinKv1@Gmail.com>
2025-09-04 18:55:27 +0800

    refactor(start.sh): 修改启动脚本以使用相对路径
    - 添加获取脚本所在目录的方法，确保脚本在任何位置都能正确运行
    - 使用相对路径启动 pytracking_server.py，提高脚本的可移植性

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
-#!/bin/bash
-cd ~/pytracking
-uv run ~/pytracking/interfaces/pytracking_server.py
+#!/bin/bash# 
+
+#获取脚本所在目录的方法
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+echo "脚本所在目录: $SCRIPT_DIR"
+
+cd "$SCRIPT_DIR"
+uv run ./interfaces/pytracking_server.py
